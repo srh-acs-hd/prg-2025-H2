bc9f2fbca61bd202d241f1ed29ffa471019f1096
srh-acs-hd <paul.tanzer@googlemail.com>
2025-12-11 11:16:41 +0100

    update of system out

--- JavaExample/src/main/java/HelloCourse.java
+++ JavaExample/src/main/java/HelloCourse.java
@@ -3,6 +3,5 @@ public class HelloCourse {
     public static void main(String[] args) {
         System.out.println("Hello, Programming Course!");
         System.out.println("Congratulations your first java programming is running");
-        System.out.println("And now we go on break for 10  minutes");
     }
 }
