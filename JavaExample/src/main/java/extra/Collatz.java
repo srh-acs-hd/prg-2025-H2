a23513f5d4e9981b8a78893853cd960d2d4547ad
Applied Computer Science <peter.dillinger2@srh.de>
2026-01-29 15:23:58 +0100

    improved version to find biggest Collatz sequence

--- JavaExample/src/main/java/extra/Collatz.java
+++ JavaExample/src/main/java/extra/Collatz.java
@@ -8,14 +8,24 @@ public class Collatz {
         Q2:  Determine the loop count for some input of n
         Q3:  What is the maximum loop count for all positive n > 0 ?
     */
+
+    private static final int MAX_N_VALUE = 1_000_000_000;
     public static void main(String[] args) {
         long maxLoopCount = 0;
         long numberWithMaxLoopCount = 0;
+        int[] memory = new int[MAX_N_VALUE]; // array will be initialized to 0
 
-        for (int i=1; i<=Integer.MAX_VALUE; i++) {
+        long startTime = System.currentTimeMillis();
+        for (int i=1; i<=MAX_N_VALUE; i++) {
             long n = i;
+
             long loopCount = 0;
             while (n != 1) {
+                if (n < MAX_N_VALUE && memory[ (int)n-1 ] > 0) {
+                    loopCount += memory[ (int)n-1 ];
+                    break;
+                }
+
                 if (n % 2 == 0) {
                     n = n/2;
                 }
@@ -24,6 +34,7 @@ public class Collatz {
                 }
                 loopCount++;
             }
+            memory[i-1] = (int)loopCount;
 
             if (loopCount > maxLoopCount) {
                 System.out.println(i + " -> " + loopCount);
@@ -31,6 +42,8 @@ public class Collatz {
                 numberWithMaxLoopCount = i;
             }
         }
+        long endTime = System.currentTimeMillis();
         System.out.println("Maximum loop count is " + maxLoopCount + " for number " + numberWithMaxLoopCount);
+        System.out.println("Execution time: " + (endTime - startTime) + " ms");
     }
 }
